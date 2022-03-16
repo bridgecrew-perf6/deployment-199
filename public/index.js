13735b38a116787e4ce1e01e4dfa1a6dec6b7416
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-16 15:39:11 -0600

    edited styling and add more to section1

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const wantBtn = document.getElementById('want')
 
 function wantAlert(event) {
     event.preventDefault()
-    alert('apparently this whole website is selling a false product')
+    alert('apparently i lied')
 }
 
 wantBtn.addEventListener('click', wantAlert)
