4b10ba0cebf9cd75d52536523eb20c818b4e1b0e
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-17 14:07:57 -0600

    added rollbar functionality

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const app = express ()
 app.use(cors())
 app.use(express.json())
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '96d971dbbb004c858772612e6b9be77d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 
 //Middleware
 app.use(express.static(path.join(__dirname, "../public")))
